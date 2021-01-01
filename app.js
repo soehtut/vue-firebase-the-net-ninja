1dbbdb0e67c7569b1994ce2f9d4dccf763ebcdbd
SoeHtut <soehtut@escapepixel.com>
2021-01-01 19:50:05 +0630

    Other Mouse Events

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ const app = Vue.createApp({
             power: 'God of Thunder',
             weapon: 'Hammer',
             age: 30,
-            isAvenger: true
+            isAvenger: true,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleAvenger() {
             this.isAvenger = !this.isAvenger
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if(data) {
+                console.log(data)
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 });
