70b113e4e268433826b50b48738cdb4cf1ed7b7c
SoeHtut <soehtut@escapepixel.com>
2021-01-01 15:46:57 +0630

    Templates & Data

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
     // data, functions
-    template: '<h2>This is the template</h2>'
+    // template: '<h2>This is the template</h2>'
+    data() {
+        return {
+            name: 'Thor',
+            power: 'God of Thunder',
+            weapon: 'Hammer'
+        }
+    }
 });
 
 app.mount("#app")
